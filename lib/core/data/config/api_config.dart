eadd77af5bdda2027c35be7e47a480a54a2395a6
Riccardo Calligaro <riccardocalligaro@gmail.com>
2021-01-21 10:52:02 +0100

    Change app icon

--- lib/core/data/config/api_config.dart
+++ lib/core/data/config/api_config.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 class ApiConfig {
-  static String apiUrl = "http://8a28160c3e6e.eu.ngrok.io";
-  // static String apiUrl =
-  //     Platform.isAndroid ? 'http://10.0.2.2:8080' : 'http://127.0.0.1:8080';
+  // static String apiUrl = "http://a7f7c1f040de.eu.ngrok.io";
+  static String apiUrl =
+      Platform.isAndroid ? 'http://10.0.2.2:8080' : 'http://127.0.0.1:8080';
 
   // static const String API_URL = 'http://10.0.2.2:8000';
 }
